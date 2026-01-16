291bec986b817b904b3e95497cadb3db41c517ee
Dmytro Somliev <somleff@gmail.com>
2026-01-16 15:53:35 +0100

    More syntax and loops

--- cards/deck.go
+++ cards/deck.go
@@ -4,6 +4,20 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+	cardsSuits := []string{"Spades", "Diamonds", "Harts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Tree", "Four"}
+
+	for _, suit := range cardsSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
--- cards/main.go
+++ cards/main.go
@@ -1,13 +1,7 @@
 package main
 
 func main() {
-	cards := deck{"Ace of Spade", newCard()}
-	cards = append(cards, "Six of Spades")
+	cards := newDeck()
 
 	cards.print()
-
-}
-
-func newCard() string {
-	return "card name"
 }
