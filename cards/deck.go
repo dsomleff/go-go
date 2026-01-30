51cca4aaeb63cf4038170e9639292918994a2dd8
Dmytro Somliev <somleff@gmail.com>
2026-01-30 17:02:44 +0100

    saveToFile, read from file, errors

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"fmt"
 	"strings"
 )
@@ -33,4 +34,20 @@ func deal(d deck, handSize int) (deck, deck) {
 
 func (d deck) toString() string{
 	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error  {
+	return os.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := os.ReadFile(filename)
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
 }
