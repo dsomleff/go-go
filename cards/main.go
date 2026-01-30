4b7e2024041e8357cc2d5d45e780629932b745de
Dmytro Somliev <somleff@gmail.com>
2026-01-30 11:40:03 +0100

    Byte slice, type to string, join slice of strings

--- cards/main.go
+++ cards/main.go
@@ -1,10 +1,8 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
-
-	hand, remainedDeck := deal(cards, 5)
-
-	hand.print()
-	remainedDeck.print()
+	fmt.Println(cards.toString())
 }
