675752e59482657f450c73b8b2e4d1930e3364ec
Dmytro Somliev <somleff@gmail.com>
2026-02-10 14:28:56 +0100

    shuffle func

--- cards/deck.go
+++ cards/deck.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"os"
 	"fmt"
+	"math/rand"
+	"os"
 	"strings"
 )
 
@@ -50,4 +51,10 @@ func newDeckFromFile(filename string) deck {
 
 	s := strings.Split(string(bs), ",")
 	return deck(s)
+}
+
+func (d deck) shuffle() {
+	rand.Shuffle(len(d), func(i, j int) {
+		d[i], d[j] = d[j], d[i]
+	})
 }
--- cards/main.go
+++ cards/main.go
@@ -1,6 +1,7 @@
 package main
 
 func main() {
-	cards := newDeckFromFile("myCards")
- 	cards.print()
+	cards := newDeck()
+	cards.shuffle()
+	cards.print()
 }
